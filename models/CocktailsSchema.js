86a99b2b053c5a0f34683ef545fad03b3cf99c95
Kévin <kevin.joya@outlook.com>
2021-11-12 18:48:53 +0100

    Created cocktails schema

---
+++ models/CocktailsSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+// const {Schema, model } = require('mongoose');
+
+const CocktailsSchema = new Schema({
+  name: String,
+  description: String,
+  img: String,
+  ingredients: Array,
+  recipe: Array,
+})
+
+  module.exports = mongoose.model('Cocktails', CocktailsSchema);
