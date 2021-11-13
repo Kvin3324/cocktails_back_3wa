9201f1d5f04084bbdff51b8ad7355e5001dc8018
Kévin <kevin.joya@outlook.com>
2021-11-13 12:34:09 +0100

    Deleted file

--- models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-  const UserSchema = new mongoose.Schema({
-    email: String,
-    password: String,
-    confirm_password: String
-  })
-
-  module.exports = mongoose.model('user', UserSchema)
